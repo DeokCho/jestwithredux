4cd58d24a7cf8a60d7336526cfecf70445e99519
DeokCho <stargazer61@naver.com>
2021-12-12 18:21:37 +0900

    JEST 적용

--- jestwithredux/src/App.test.js
+++ jestwithredux/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import renderer from "react-test-renderer";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("renders learn react link", () => {
+ let component = null;
+ it("renders correctly", () => {
+  component = renderer.create(<App />);
+ });
+ it("matches snapshot", () => {
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+ });
 });
